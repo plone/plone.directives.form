449a068fbd49a9c01e52e76991a7b58f58195ba5
Maurits van Rees <maurits@vanrees.org>
2015-11-28 01:28:45 +0100

    Added classifiers.

--- setup.py
+++ setup.py
@@ -14,7 +14,11 @@ setup(name='plone.directives.form',
       # https://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
           "Framework :: Plone",
+          "Framework :: Plone :: 4.3",
+          "Framework :: Plone :: 5.0",
           "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
           "Topic :: Software Development :: Libraries :: Python Modules",
           ],
       keywords='grok plone dexterity form content',
